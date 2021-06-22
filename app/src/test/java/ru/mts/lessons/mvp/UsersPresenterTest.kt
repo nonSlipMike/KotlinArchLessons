871e30f824981382695df7bf858ce2782bddb776
Mikle Pashenko <mypashhe@mts.ru>
2021-06-22 05:02:13 +0300

    add interfaces and tests update 1

--- app/src/test/java/ru/mts/lessons/mvp/UsersPresenterTest.kt
+++ app/src/test/java/ru/mts/lessons/mvp/UsersPresenterTest.kt
@@ -14,7 +14,6 @@ class UsersPresenterTest {
         val presenter = UsersPresenter(model)
 
         every { view.getUsers() } returns UserData("111", "111")
-        every { view.showProgress() } just Runs
         every { model.addUser(any(), any()) } answers { presenter.loadUsers() }
         every { model.loadUsers(any()) } answers { view.showUsers(listOf(User(1L, "111", "111"))) }
 
